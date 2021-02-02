93808b2c92853306b8942d0784ba9aed46876ead
Davidedes <44806393+Davidedes@users.noreply.github.com>
2021-02-02 21:39:56 +0100

    Aggiunti altri input e varie modifiche al css

--- message.php
+++ message.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__.'/proxy/checkToken.php';
+require __DIR__ . '/proxy/checkToken.php';
 $style = "<link href=\"assets/css/message.css\" rel=\"stylesheet\">";
 $page_title = "Messaggi";
 require 'layouts/head.php';
@@ -8,81 +8,101 @@ require 'layouts/head.php';
 <body class="text-center">
 <form id="SelectUser" name="SelectUser">
     <div class="container">
-        <div class="row mt-3">
-            <div class="col-sm-4"></div>
-            <div class="col-sm-2"><input type="radio" name="Select" onclick="SetAllCheckBoxes('SelectUser', 'myCheckBox', true);"> Seleziona tutto</div>
-            <div class="col-sm-2"><input type="radio" checked name="Select" onclick="SetAllCheckBoxes('SelectUser', 'myCheckBox', false);"> Deseleziona tutto</div>
-            <div class="col-sm-4"></div>
-        </div>
-        <div class="row mt-3">
-
-        </div>
-        <div class="row mt-3">
-            <div class="col-sm-3"></div>
-            <div class="col-sm-3">
-                <div class="card mb-3 text-center">
-                    <div class="row g-0">
-                        <div class="col-md-2" style=" margin:auto 0">
-                            <i class="fas fa-user fa-lg"></i>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <p class="card-text">
-                                    Vita Barletta
-                                </p>
-                            </div>
-                        </div>
-                        <div class="col-md-2 form-check">
-                            <input type="checkbox" name="myCheckBox">
-                        </div>
-
-                    </div>
+        <fieldset class="border mt-3 p-2">
+            <legend>Seleziona le chat</legend>
+            <div class="row mt-3">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-2"><input type="radio" name="Select"
+                                             onclick="SetAllCheckBoxes('SelectUser', 'myCheckBox', true);"> Seleziona
+                    tutto
                 </div>
+                <div class="col-sm-2"><input type="radio" checked name="Select"
+                                             onclick="SetAllCheckBoxes('SelectUser', 'myCheckBox', false);"> Deseleziona
+                    tutto
+                </div>
+                <div class="col-sm-4"></div>
             </div>
-            <div class="col-sm-3">
-                <div class="card mb-3 text-center">
-                    <div class="row g-0">
-                        <div class="col-md-2" style=" margin:auto 0">
-                            <i class="fas fa-user fa-lg"></i>
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <p class="card-text">
-                                    Vita Barletta
-                                </p>
+            <div class="row mt-3">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-3">
+                    <div class="card mb-3 text-center">
+                        <div class="row g-0">
+                            <div class="col-md-2" style=" margin:auto 0">
+                                <i class="fas fa-user fa-lg"></i>
                             </div>
-                        </div>
-                        <div class="col-md-2 form-check">
-                            <input type="checkbox" name="myCheckBox">
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Vita Barletta
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-2 form-check">
+                                <input type="checkbox" name="myCheckBox">
+                            </div>
+
                         </div>
                     </div>
                 </div>
+                <div class="col-sm-3">
+                    <div class="card mb-3 text-center">
+                        <div class="row g-0">
+                            <div class="col-md-2" style=" margin:auto 0">
+                                <i class="fas fa-user fa-lg"></i>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Vita Barletta
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-2 form-check">
+                                <input type="checkbox" name="myCheckBox">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3"></div>
             </div>
-            <div class="col-sm-3"></div>
-        </div>
+        </fieldset>
+        <fieldset class="border mt-3 p-2">
+            <legend>Seleziona qualcosa ma non so che nome darti</legend>
+            <div class="row mt-4">
+                <div class="col-sm-3"></div>
+                <div class="col-sm-3"><label for="dataInizio">Data inizio:</label> <input id="dataInizio" type="date" value="<?php echo date('Y-m-d');?>" min='1999-01-01' max="<?php echo date('Y-m-d'); ?>">
+                </div>
+                <div class="col-sm-3"><label for="dataFine">Data fine:</label> <input id="dataFine" type="date" value= <?php echo date('Y-m-d'); ?> min='1999-01-01' max="<?php echo date('Y-m-d'); ?>"></div>
+                <div class="col-sm-3"></div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-sm-5" style="text-align: right">Includere i media:</div>
+                <div class="col-sm-1"><input type="radio" name="Media"> Si</div>
+                <div class="col-sm-1"><input type="radio" checked name="Media"> No</div>
+                <div class="col-sm-5"></div>
+            </div>
+        </fieldset>
     </div>
 </form>
 <?php require('layouts/scripts.php'); ?>
 <script src="./assets/js/index.js"></script>
 </body>
 </html>
-<<<<<<< Updated upstream
-=======
+
 
 <script type="text/javascript">
-    function SetAllCheckBoxes(FormName, FieldName, CheckValue)
-    {
-        if(!document.forms[FormName])
+    function SetAllCheckBoxes(FormName, FieldName, CheckValue) {
+        if (!document.forms[FormName])
             return;
         var objCheckBoxes = document.forms[FormName].elements[FieldName];
-        if(!objCheckBoxes)
+        if (!objCheckBoxes)
             return;
         var countCheckBoxes = objCheckBoxes.length;
-        if(!countCheckBoxes)
+        if (!countCheckBoxes)
             objCheckBoxes.checked = CheckValue;
         else
-            for(var i = 0; i < countCheckBoxes; i++)
+            for (var i = 0; i < countCheckBoxes; i++)
                 objCheckBoxes[i].checked = CheckValue;
     }
 </script>
->>>>>>> Stashed changes
+
