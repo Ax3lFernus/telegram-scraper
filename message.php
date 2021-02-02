e10181f4981e557f604af31bfb9e38501329d935
Davidedes <44806393+Davidedes@users.noreply.github.com>
2021-02-02 20:26:09 +0100

    Aggiunto il seleziona tutto

--- message.php
+++ message.php
@@ -6,57 +6,83 @@ require 'layouts/head.php';
 ?>
 
 <body class="text-center">
-
-<div class="container">
-<div class="row mt-3">
-    <div class="col-sm-2"></div>
-    <div class="col-sm-8">
-        <div class="card card-active-listener text-center" style="border-radius: 20px;">
-            <div class="card-body">
-
-                <div class="media">
-
-
-                    <div class="avatar mr-5">
-                        <img class="avatar-img" src="assets/images/avatars/11.jpg" alt="Bootstrap Themes">
-                    </div>
-
-                    <div class="media-body overflow-hidden">
-                        <div class="d-flex align-items-center mb-1">
-                            <h6 class="text-truncate mb-0 mr-auto">Bootstrap Themes</h6>
-                            <p class="small text-muted text-nowrap ml-4">10:42 am</p>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+<form id="SelectUser" name="SelectUser">
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-2"><input type="radio" name="Select" onclick="SetAllCheckBoxes('SelectUser', 'myCheckBox', true);"> Seleziona tutto</div>
+            <div class="col-sm-2"><input type="radio" checked name="Select" onclick="SetAllCheckBoxes('SelectUser', 'myCheckBox', false);"> Deseleziona tutto</div>
+            <div class="col-sm-4"></div>
         </div>
-    </div>
-    <div class="col-sm-2"></div>
-</div>
-    <div class="row mt-3">
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-            <div class="card mb-3 text-center" style="border-radius: 20px;">
-                <div class="row g-0">
-                    <div class="col-md-1">
-                        <i class="fas fa-user"></i>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <p class="card-text" style="text-align: left">
-                                Vita Barletta
-                            </p>
+        <div class="row mt-3">
+
+        </div>
+        <div class="row mt-3">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3">
+                <div class="card mb-3 text-center">
+                    <div class="row g-0">
+                        <div class="col-md-2" style=" margin:auto 0">
+                            <i class="fas fa-user fa-lg"></i>
                         </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Vita Barletta
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 form-check">
+                            <input type="checkbox" name="myCheckBox">
+                        </div>
+
                     </div>
                 </div>
             </div>
+            <div class="col-sm-3">
+                <div class="card mb-3 text-center">
+                    <div class="row g-0">
+                        <div class="col-md-2" style=" margin:auto 0">
+                            <i class="fas fa-user fa-lg"></i>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Vita Barletta
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-2 form-check">
+                            <input type="checkbox" name="myCheckBox">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3"></div>
         </div>
-        <div class="col-sm-3"></div>
     </div>
-</div>
-
+</form>
 <?php require('layouts/scripts.php'); ?>
 <script src="./assets/js/index.js"></script>
 </body>
 </html>
+<<<<<<< Updated upstream
+=======
+
+<script type="text/javascript">
+    function SetAllCheckBoxes(FormName, FieldName, CheckValue)
+    {
+        if(!document.forms[FormName])
+            return;
+        var objCheckBoxes = document.forms[FormName].elements[FieldName];
+        if(!objCheckBoxes)
+            return;
+        var countCheckBoxes = objCheckBoxes.length;
+        if(!countCheckBoxes)
+            objCheckBoxes.checked = CheckValue;
+        else
+            for(var i = 0; i < countCheckBoxes; i++)
+                objCheckBoxes[i].checked = CheckValue;
+    }
+</script>
+>>>>>>> Stashed changes
