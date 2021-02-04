b891914f803e37ade5c90b89a7909fa92cf8d081
Alessandro Annese <a.annese99@gmail.com>
2021-02-04 13:04:42 +0100

    Rimossa l'inclusione di account eliminati

--- proxy/getDialogs.php
+++ proxy/getDialogs.php
@@ -1,22 +1,24 @@
 <?php
-if(isset($_COOKIE['token'])) {
+if (isset($_COOKIE['token'])) {
     $token = $_COOKIE['token'];
     $chats = curl($baseUrl . 'api/users/' . $token . '/getFullDialogs');
     $chat_list = array();
-    if($chats->success) {
-        foreach ($chats->response as $id=>$chat) {
-            $info = curl($baseUrl . 'api/users/' . $token . '/getInfo?peer=' . $id );
+    if ($chats->success) {
+        foreach ($chats->response as $id => $chat) {
+            $info = curl($baseUrl . 'api/users/' . $token . '/getInfo?peer=' . $id);
             if (key($info->response) == 'Chat') {
                 //Chat di gruppo/Canali
                 $name = $info->response->Chat->title;
             } else {
                 //Utente/Bot
-                if(isset($info->response->User->first_name)){
+                if (isset($info->response->User->first_name)) {
                     $name = $info->response->User->first_name;
-                }else{
+                } elseif (isset($info->response->User->username)) {
                     $name = "@" . $info->response->User->username;
+                } else {
+                    continue;
                 }
-                if(isset($info->response->User->last_name)){
+                if (isset($info->response->User->last_name)) {
                     $name .= " " . $info->response->User->last_name;
                 }
             }
