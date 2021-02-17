3dbd78c1572761aae452c81ac25ce51b42ea187a
Alessandro Annese <a.annese99@gmail.com>
2021-02-17 23:46:42 +0100

    Aggiunta doppio hash e utenti nei gruppi [BACKEND]

--- proxy/downloadMediaStatus.php
+++ proxy/downloadMediaStatus.php
@@ -3,8 +3,8 @@ require __DIR__ . '/functions.php';
 
 header('Content-Type: application/json');
 if (isset($_COOKIE['token']) && isset($_GET['media_num']) && isset($_GET['zip_name'])) {
-    $tmpDir = dirname(__DIR__, 1) . '/tmp';
-    $downloadDir = $tmpDir . '/' . $_COOKIE['token'];
+    $tmpDir = dirname(__DIR__, 1) . '/tmp/' . $_COOKIE['token'];
+    $downloadDir = $tmpDir . '/medias';
     if(file_exists($downloadDir)) {
         $files = scandir($downloadDir);
         natsort($files);
@@ -13,7 +13,7 @@ if (isset($_COOKIE['token']) && isset($_GET['media_num']) && isset($_GET['zip_na
         echo '{"percentage": ' . round($percentage * 100) . ', "status" : false}';
     }else{
         if(file_exists($tmpDir . '/' . $_GET['zip_name']))
-            echo '{"percentage": 100, "status" : true}';
+            echo '{"percentage": 100, "status" : true, "md5": "'. hash_file('md5', $tmpDir . '/' . $_GET['zip_name']) . '", "sha256": "' . hash_file('sha256', $tmpDir . '/' . $_GET['zip_name']) . '"}';
         else
             echo '{"percentage": 0, "status" : false}';
     }
--- proxy/getMessages.php
+++ proxy/getMessages.php
@@ -2,7 +2,7 @@
 ob_start();
 require __DIR__ . '/functions.php';
 
-if (isset($_COOKIE['token']) && isset($_POST['chats']) && isset($_POST['media'])) {
+if (isset($_COOKIE['token']) && isset($_POST['chats']) && isset($_POST['media']) && isset($_POST['users_groups']) && isset($_POST['filetype'])) {
     $token = $_COOKIE['token'];
     $chats = $_POST['chats'];
     $dataInizio = $_POST['dataInizio'];
@@ -10,15 +10,39 @@ if (isset($_COOKIE['token']) && isset($_POST['chats']) && isset($_POST['media'])
     $dataFine = date("Y-m-d", strtotime("$dataFine +1 day"));
     $messages = array(array('chat_id', 'chat_name', 'author', 'date', 'message', 'media_name'));
     $users_info = array();
+    $users_in_groups = array(array('chat_id', 'chat_name', 'chat_type', 'user_id', 'first_name', 'last_name', 'username', 'join_date', 'role'));
     $getMedia = $_POST['media'] == 0 ? false : true;
+    $getUsersInGroups = $_POST['users_groups'] == 0 ? false : true;
     $zipName = 'null';
     $media_id = 0;
+
+    $tmpDir = dirname(__DIR__, 1) . '/tmp/' . $token;
+
+    if (file_exists($tmpDir)) {
+        array_map('unlink', array_filter((array)glob($tmpDir . "/*")));
+        rmdir($tmpDir);
+    }
+    mkdir($tmpDir, 0777, true);
+
     if ($getMedia) {
         $media = array();
         $zipName = generateRandomString(15);
     }
     foreach ($chats as $chat) {
         $offset_msg_id = 0;
+        if ($getUsersInGroups) {
+            $chat_info = curl($baseUrl . 'api/users/' . $token . '/getPwrChat?id=' . $chat['id']);
+            if (isset($chat_info->response->participants)) {
+                foreach ($chat_info->response->participants as $participant) {
+                    $first_name = isset($participant->user->first_name) ? $participant->user->first_name : '';
+                    $last_name = isset($participant->user->last_name) ? $participant->user->last_name : '';
+                    $username = isset($participant->user->username) ? $participant->user->username : '';
+                    $date = isset($participant->date) ? $participant->date : 0;
+                    array_push($users_in_groups, [$chat_info->response->id, $chat_info->response->title, $chat_info->response->type, $participant->user->id, $first_name, $last_name, $username, $date, $participant->role]);
+                }
+            }
+        }
+
         do {
             $chat_messages = curl($baseUrl . 'api/users/' . $token . '/messages.getHistory?data[peer]=' . $chat['id'] . '&data[offset_id]=' . $offset_msg_id . '&data[offset_date]=' . strtotime($dataFine) . '&data[add_offset]=0&data[limit]=100&data[max_id]=0&data[min_id]=0');
             if (count($chat_messages->response->messages) <= 0) break;
@@ -47,7 +71,32 @@ if (isset($_COOKIE['token']) && isset($_POST['chats']) && isset($_POST['media'])
             sleep(2);
         } while (true);
     }
-    echo json_encode(array($messages, $zipName . '.zip', $media_id));
+    if ($_POST['filetype']) {
+        //CSV
+        $file_ext = '.csv';
+        $messages_csv = fopen($tmpDir . '/messages.csv', 'w');
+        foreach ($messages as $fields) {
+            fputcsv($messages_csv, $fields);
+        }
+        fclose($messages_csv);
+        $users_in_groups_csv = fopen($tmpDir . '/users_in_groups.csv', 'w');
+        foreach ($users_in_groups as $fields) {
+            fputcsv($users_in_groups_csv, $fields);
+        }
+        fclose($users_in_groups_csv);
+    } else {
+        //JSON
+        $file_ext = '.json';
+    }
+
+    echo json_encode(['messages' => ['url' => './tmp/' . $token . '/messages' . $file_ext,
+        'md5' => hash_file('md5', $tmpDir . '/messages' . $file_ext),
+        'sha256' => hash_file('sha256', $tmpDir . '/messages' . $file_ext)],
+        'users_in_groups' => $getUsersInGroups ? ['url' => './tmp/' . $token . '/users_in_groups' . $file_ext,
+            'md5' => hash_file('md5', $tmpDir . '/users_in_groups' . $file_ext),
+            'sha256' => hash_file('sha256', $tmpDir . '/users_in_groups' . $file_ext)] : null,
+        'media' => $getMedia ? ['zip_name' => $zipName . '.zip', 'num_media' => $media_id] : null]);
+
     $size = ob_get_length();
     header('Content-Type: application/json');
     header("Content-Encoding: none");
