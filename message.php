4ca54ce99eba6cf824c109caacf7593f75b74173
Alessandro Annese <a.annese99@gmail.com>
2021-03-01 19:39:19 +0100

    Modifica minore

--- message.php
+++ message.php
@@ -161,7 +161,7 @@ require 'layouts/head.php';
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Doppio Hash</h5>
+                <h5 class="modal-title">Download completato</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
